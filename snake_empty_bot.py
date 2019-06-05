012976367c403326f28dfaa0c425544b591722e4
Joel Potts <joel.th.potts@gmail.com>
2019-06-05 12:42:06 +0100

    removed functions

--- snake_empty_bot.py
+++ snake_empty_bot.py
@@ -25,36 +25,15 @@ def calculate_move(gamestate):
 
 
 def avoid_walls(directions, board, snake):
-    moves = []
-    for direction in directions:
-        new_x = snake[0][0] + dirs[direction][0]
-        new_y = snake[0][1] + dirs[direction][1]
-        if new_x >= 0 and new_x < len(board[0]) and new_y >= 0 and new_y < len(board):
-            moves.append(direction)
-    return moves
+    return directions
     
 def avoid_snakes(directions, board, snake, opp_snake, food):
-    moves = []
-    for direction in directions:
-        new_x = snake[0][0] + dirs[direction][0]
-        new_y = snake[0][1] + dirs[direction][1]
-        if board[new_y][new_x] == -1 or board[new_y][new_x] == 2 or (snake[-1] == [new_x, new_y] and len(snake) != 2) or (opp_snake[-1] == [new_x, new_y] and distance_from_coordinate(opp_snake, food)() > 1):
-            moves.append(direction)
-    print(moves)
-    return moves
+    return directions
     
 def avoid_potential_snakes(directions, snake, opp_snake):
-    moves = []
-    for direction in directions:
-        if distance_from_coordinate(snake, opp_snake[0])(direction) > 1:
-            moves.append(direction)
-    if moves == []:
-        moves = directions
-    return moves
+    return directions
     
 def rank_moves(directions, snake, food):
-    shuffle(directions)
-    directions.sort(key=distance_from_coordinate(snake, food))
     return directions
     
 def distance_from_coordinate(snake, coord):
@@ -64,6 +43,3 @@ def distance_from_coordinate(snake, coord):
         else:
             return abs(snake[0][0] + dirs[x][0] - coord[0]) + abs(snake[0][1] + dirs[x][1] - coord[1])
     return func
-
-
-
